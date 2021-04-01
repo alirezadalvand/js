d75b43700d54939d0e0984d41108b478d66822f7
alirezadalvand <alireza.d1339@gmail.com>
2021-04-02 03:52:17 +0430

    added first

---
+++ script.js
@@ -0,0 +1,70 @@
+// let a = 1;
+// const b = 2;
+
+// a = 4;
+// b = 6;
+
+/////////////////////
+
+// let name = "alireza"
+// name = "bahar"
+
+// console.log(name);
+
+//////////////////////////
+
+// const name = "cr7";
+// name = "messi";
+
+// console.log(name);
+
+//////////////////////////////////
+
+// const FirstName = "alireza";
+// let LastName = "dalvand";
+
+// const fullName = LastName + FirstName;
+
+// console.log(fullName);
+
+////////////////////////////////////////////
+
+// let number = 8;
+// number + 3;
+
+// console.log(number);
+
+/////////////////////////////////////////
+
+// let number = 125;
+// const num = 12;
+// number++;
+// console.log(number);
+
+// ////////////////////////////////////////
+
+// let num = 1;
+// let name = "1";
+
+// const all = num + name;
+
+// console.log(all);
+
+/////////////////////////////////////
+
+// choose your favorite food
+
+// const food = ["pasta" , "pizza" , "burger" , "fish" , "snack" , "kabab"]
+
+/////////////////////////////////////////////////////////
+
+// change foods to another foods
+
+// const car =
+//   "lambo is my favorite car in the world..., i love ayda and hate hanie ayda love lambo";
+
+// const myCar = car.replaceAll("lambo", "pride");
+
+// console.log(myCar);
+
+/////////////////////////////////////////////////
